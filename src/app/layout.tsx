ade069471fb823d3b556012569296eefafbf7a5a
CodeSandbox <support@codesandbox.io>
2023-09-28 16:59:40 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Notion Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
